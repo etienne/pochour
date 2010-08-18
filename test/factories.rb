33024cc3347219ded20e1b22edb383574d96e73c
Étienne Després <etienne.despres@molotovcoop.org>
2010-08-17 22:24:30 -0400

    Réglé un bug qui empêchait le feature Manage Articles de passer.

--- test/factories.rb
+++ test/factories.rb
@@ -1,13 +1,17 @@
+Factory.sequence :email do |n|
+  "roger#{n}@example.com"
+end
+
 Factory.define :user do |u|
   u.name 'Steve Fields'
-  u.email 'steve@fields.com'
+  u.email { Factory.next(:email) }
   u.password 'fieldsfields'
   u.admin false
 end
 
 Factory.define :admin, :class => User do |a|
   a.name 'Jésus'
-  a.email 'jesus@voujvou.com'
+  a.email { Factory.next(:email) }
   a.password 'jesusjesus'
   a.admin true
 end
@@ -15,4 +19,5 @@ end
 Factory.define :article do |a|
   a.title "Trop malade ton titre"
   a.article "Fucké raide là"
+  a.association :user
 end
