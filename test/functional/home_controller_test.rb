632875dc13600e6620b20aa6d598d096fe5b5bb9
Étienne Després <etienne.despres@molotovcoop.org>
2010-08-27 20:48:27 -0400

    Ajouté un test pour HomeController.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  context "GET to :index" do
+    setup do
+      get :index
+    end
+
+    should respond_with(:success)
+    should render_template(:index)
+    should_not set_the_flash
+  end
 end
