ac4fb8aa6e7f25cefe0bd26f8658fb1cb10c5d75
Étienne Després <etienne.despres@molotovcoop.org>
2010-08-28 18:37:37 -0400

    Bugfix.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,8 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     comment = params[:comment]
     if user_signed_in? and current_user.name == comment[:author]
-      comment.user = current_user
-      comment.author = nil
+      comment[:user] = current_user
+      comment[:author] = nil
     end
     @comment = @article.comments.create(comment)
     redirect_to @article
