853f88409038b0eab510bde2460031501e6139f4
Étienne Després <etienne.despres@molotovcoop.org>
2011-01-25 21:55:33 -0500

    Modifié le fonctionnement des clickable_items.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
   // Make specified <li>’s clickable. Use the first <a> as link target.
   $('ul.clickable_items li').each(function(item) {
-    $(this).mouseover(function() {
-      $(this).addClass('hover');
-    }).mouseout(function() {
-      $(this).removeClass('hover');
-    }).click(function() {
+    $(this).click(function() {
       location.href = $(this).find('a').first().attr('href');
     })
 	});
