8a10d0bbd02b0c45a841ed761ecf1f594135fe97
Étienne Després <etienne.despres@molotovcoop.org>
2012-03-10 23:31:47 -0500

    Remove forms. Prepare for initial launch.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
+# encoding: UTF-8
+
 class HomeController < ApplicationController
   def index
     @articles = Article.where(:draft => false).order("created_at DESC").limit(18)
+    @slogan = [
+      "Des affaires.",
+      "C’était vraiment long.",
+      "Trop peu, trop tard.",
+      "Sans slogan depuis 1903.",
+      "On s’y croirait.",
+      "On travaille chaud.",
+      "Entrez voir nos spéciaux.",
+      "Là où la qualité n’est pas un obstacle.",
+      "Malade.",
+      "Fais-le vous-même."
+    ].sample
   end
 end
